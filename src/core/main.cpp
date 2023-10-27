65f194b82a42c71150032a1499da82fc250e3eec
lilwizzz <amar.ustavdija@gmail.com>
2023-10-28 00:43:00 +0200

    managed to display a window, main.cpp

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,6 +1,45 @@
-#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-int main(int, char**) {
+int main() {
+    
+    if (!glfwInit()) {
+        return -1;
+    }
 
-    std::cout << "Hello, from Fuze!\n";
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Fuze", NULL, NULL);
+    if (!window) {
+        // Window creation failed
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's OpenGL context current
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        // GLEW initialization failed
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        // Game loop code goes here
+        
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        // Swap the front and back buffers
+        glfwSwapBuffers(window);
+
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW and clean up resources
+    glfwTerminate();
+
+    return 0;
 }
